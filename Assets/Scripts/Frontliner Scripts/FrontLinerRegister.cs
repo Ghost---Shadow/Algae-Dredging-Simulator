5f55af5d7569b7516ce2f6d08bfe57dffbf8b50c
Ghost---Shadow <souradeep.nanda2014@vit.ac.in>
2016-10-30 08:28:19 +0530

    Grid system without blocking tiles

--- Assets/Scripts/Frontliner Scripts/FrontLinerRegister.cs
+++ Assets/Scripts/Frontliner Scripts/FrontLinerRegister.cs
@@ -1,24 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FrontLinerRegister : MonoBehaviour {
+public class FrontLinerRegister : MonoBehaviour
+{
+    public int sizeX = 10;
+    public int sizeY = 10;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public int[,] map;
 
-	public void requestWaypoints(FrontLinerScript bot){
-		Vector3[] wps = new Vector3[3];
-		wps[0] = new Vector3(8,0,-8);
-		wps[1] = new Vector3(8,0,8);
-		wps[2] = new Vector3(-8,0,8);
+    public static int UNEXPLORED = 0;
+    public static int DIRTY = 1;
+    public static int CLEAN = 2;
+	public static int BLOCKED = 3;
 
-		bot.setWaypoints(wps);
-	}
+    void Start()
+    {
+        reset();
+    }
+
+    public void reset()
+    {
+        map = new int[sizeX, sizeY];
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                map[i, j] = UNEXPLORED;
+            }
+        }
+    }
+
+    public void requestWaypoints(FrontLinerScript bot)
+    {
+        Vector3[] wps = new Vector3[3];
+        wps[0] = new Vector3(8, 0, -8);
+        wps[1] = new Vector3(8, 0, 8);
+        wps[2] = new Vector3(-8, 0, 8);
+
+        bot.setWaypoints(wps);
+    }
 }
+
--- Assets/Scripts/Global Scripts/WaterColorScript.cs
+++ Assets/Scripts/Global Scripts/WaterColorScript.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class WaterColorScript : MonoBehaviour
 {
-    public Material unexplored, dirty, clean;
+    public Material[] materials;
+    public int x,y;
+
     private bool isExplored;
+    private FrontLinerRegister registry;
 
     void Start()
     {
-        GetComponent<Renderer>().material = unexplored;
+        //GetComponent<Renderer>().material = unexplored;
+        registry = GameObject.Find("FrontlinerContainer").GetComponent<FrontLinerRegister>();
         isExplored = false;
+        x = ((int)transform.position.x + (registry.sizeX-1))/2;
+        y = ((int)transform.position.z + (registry.sizeY-1))/2;
+    }
+
+    void Update(){
+        GetComponent<Renderer>().material = materials[registry.map[x,y]];
     }
 
     void OnTriggerStay(Collider other)
@@ -18,12 +28,12 @@ public class WaterColorScript : MonoBehaviour
             isExplored = true;
         if (isExplored)
             if (other.gameObject.tag.Equals("ALGAE_TAG"))
-                GetComponent<Renderer>().material = dirty;
+                registry.map[x,y] = FrontLinerRegister.DIRTY;
     }
 
     void OnTriggerExit(Collider other)
     {
         if (isExplored && other.gameObject.tag.Equals("ALGAE_TAG"))
-            GetComponent<Renderer>().material = clean;
+            registry.map[x,y] = FrontLinerRegister.CLEAN;
     }
 }
